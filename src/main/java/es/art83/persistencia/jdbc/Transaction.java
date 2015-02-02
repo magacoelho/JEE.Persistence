b78878a96e22782c2d0f1f50de82a436dc0bb93d
miw-upm <miw@eui.upm.es>
2015-02-02 13:25:10 +0100

    Transaction

--- src/main/java/es/art83/persistencia/jdbc/Transaction.java
+++ src/main/java/es/art83/persistencia/jdbc/Transaction.java
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class Transaccion {
+public class Transaction {
     public static void main(String[] args) {
         Connection conexion = null;
         Statement sentencia = null;
